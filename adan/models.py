3ab7f31668586e5269824b20542c219f705b79e7
S-Amine <saidi.amine.p@gmail.com>
2022-12-20 16:14:45 +0100

    remove unused models

--- adan/models.py
+++ adan/models.py
@@ -1,14 +1,34 @@
-from django.db import models
-from django.contrib.auth.models import User
-import json
-from django.core.serializers.json import DjangoJSONEncoder
 
+
+from django.db import models
+import json
+import mutagen
 from constance import config
 from datetime import datetime, timedelta
+PRAYER= (('elfajer','elfajer'),('duhr','duhr'),('alasr','alasr'),('almaghreb','almaghreb'),('alaicha','alaicha'))
+
+class PrayerAudio(models.Model):
+    audio = models.FileField("audio file")
+    prayer = models.CharField(max_length=50,choices=PRAYER)
+    audio_duration = models.PositiveIntegerField(null=True,blank=True)
+    def save(self, *args, **kwargs):
+       if self.audio_duration is None or self.audio_duration == "":
+        audio_info = mutagen.File(self.audio).info
+        self.audio_duration=int(audio_info.length)
+       super(PrayerAudio, self).save(*args, **kwargs) # Call the real save() method
+    def __str__(self):
+        return self.prayer
+
 class LiveEvent(models.Model):
     """This event is triggered immediately"""
     audio = models.FileField("audio file")
+    audio_duration = models.PositiveIntegerField(null=True,blank=True)
     created_at = models.DateTimeField(auto_now=True)
+    def save(self, *args, **kwargs):
+       if self.audio_duration is None or self.audio_duration == "":
+        audio_info = mutagen.File(self.audio).info
+        self.audio_duration=int(audio_info.length)
+       super(LiveEvent, self).save(*args, **kwargs) # Call the real save() method
     def __str__(self):
         return self.id
 
@@ -40,26 +60,18 @@ class Prayer(models.Model):
             self.prayer_time=new_dictionary
             print(alasr)
        super(Prayer, self).save(*args, **kwargs) # Call the real save() method
+
 class PrayerEvent(models.Model):
     TYPE = (('after','after'),('before','before'))
-    PRAYER= (('elfajer','elfajer'),('duhr','duhr'),('alasr','alasr'),('almaghreb','almaghreb'),('alaicha','alaicha'))
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=50,choices=TYPE)
     repeated = models.BooleanField(default=True)
     prayer = models.CharField(max_length=50,choices=PRAYER)
     audio = models.FileField(upload_to="prayer_event", max_length=250,null=True,blank=True)
+    audio_duration = models.PositiveIntegerField(null=True,blank=True)
+    def save(self, *args, **kwargs):
+       if self.audio_duration is None or self.audio_duration == "":
+        audio_info = mutagen.File(self.audio).info
+        self.audio_duration=int(audio_info.length)
+       super(PrayerEvent, self).save(*args, **kwargs) # Call the real save() method
     def __str__(self):
         return f"{self.type}-{self.prayer}"
-
-class Mosque(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
-    name = models.CharField("name of mosque", max_length=150)
-    status = models.BooleanField(default=False)
-    state = models.ForeignKey(State, on_delete=models.CASCADE,null=True,blank=True)
-    prayer = models.JSONField(null=True,blank=True)
-    def save(self, *args, **kwargs):
-        if self.prayer is None or self.prayer=="":
-            self.prayer=Prayer.objects.filter(state=self.state).first()
-        super(Mosque, self).save(*args, **kwargs) # Call the real save() method
-    def __str__(self):
-        return f"{self.name} is {self.status} now"
