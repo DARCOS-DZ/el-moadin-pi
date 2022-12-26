25529ee0cc615a35372cc30fbc599ef449fb7f7c
S-Amine <saidi.amine.p@gmail.com>
2022-12-26 21:52:36 +0100

    correct schedule way

--- adan/signals.py
+++ adan/signals.py
@@ -2,22 +2,46 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import *
 from django.conf import settings
-from .tasks import live_event_task
+
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 @receiver(post_save, sender=LiveEvent)
-def message_signal(sender, instance, **kwargs):
+def live_event_signal(sender, instance, **kwargs):
     import wget
     from pathlib import Path
     from django.core.files import File
     audio = str(instance.audio)
     try:
+        from .tasks import live_event_task, prayer_audio_task
         download  = wget.download(audio, out="media")
         downloaded_file = str(download).encode()
         path = Path(downloaded_file.decode("utf-8"))
         with path.open(mode='rb') as f:
             instance.audio = File(f, name=path.name)
             instance.save()
-        live_event_task(id=instance.id)
+        live_event_task(id=instance.id,schedule=datetime.now() )
+
+    except Exception as e:
+        pass
+
+@receiver(post_save, sender=PrayerAudio)
+def message_signal(sender, instance, **kwargs):
+    import wget
+    from pathlib import Path
+    from django.core.files import File
+    audio = str(instance.audio)
+    try:
+        from .tasks import prayer_audio_task
+        download  = wget.download(audio, out="media")
+        downloaded_file = str(download).encode()
+        path = Path(downloaded_file.decode("utf-8"))
+        with path.open(mode='rb') as f:
+            instance.audio = File(f, name=path.name)
+            instance.save()
+        date = datetime(year=2022, month=12, day=26, hour=21, minute=51, second=45)
+        # print(shecule)
+        prayer_audio_task(instance.id, schedule=date)
 
     except Exception as e:
         pass
--- adan/tasks.py
+++ adan/tasks.py
@@ -2,11 +2,12 @@ from background_task import background
 from django.conf import settings
 from .models import *
 
+
 @background(schedule=0)
 def live_event_task(id):
     # lookup user by id and send them a message
-    import pygame
     live_event = LiveEvent.objects.get(id=id)
+    import pygame
     absolute_path = str(settings.BASE_DIR) + live_event.audio.url
     pygame.mixer.init()
     pygame.mixer.music.set_volume(0.7)
@@ -14,9 +15,10 @@ def live_event_task(id):
     sound.play()
     pygame.time.wait(int(sound.get_length() * 1000))
 
-@background(schedule=10)
+@background()
 def prayer_audio_task(id):
     # lookup user by id and send them a message
+    import pygame
     prayer_audio = PrayerAudio.objects.get(id=id)
     absolute_path = str(settings.BASE_DIR) + prayer_audio.audio.url
     pygame.mixer.init()
@@ -25,9 +27,10 @@ def prayer_audio_task(id):
     sound.play()
     pygame.time.wait(int(sound.get_length() * 1000))
 
-@background(schedule=10)
+@background(schedule=0)
 def prayer_event_task(id):
     # lookup user by id and send them a message
+    import pygame
     prayer_event = PrayerEvent.objects.get(id=id)
     absolute_path = str(settings.BASE_DIR) + prayer_event.audio.url
     pygame.mixer.init()
