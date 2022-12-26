0c8dede878694af836cc620bfd259ad7ee050655
S-Amine <saidi.amine.p@gmail.com>
2022-12-26 18:53:46 +0100

    setup the tasks

--- adan/tasks.py
+++ adan/tasks.py
@@ -18,8 +18,20 @@ def live_event_task(id):
 def prayer_audio_task(id):
     # lookup user by id and send them a message
     prayer_audio = PrayerAudio.objects.get(id=id)
+    absolute_path = str(settings.BASE_DIR) + prayer_audio.audio.url
+    pygame.mixer.init()
+    pygame.mixer.music.set_volume(0.7)
+    sound = pygame.mixer.Sound(absolute_path)
+    sound.play()
+    pygame.time.wait(int(sound.get_length() * 1000))
 
 @background(schedule=10)
 def prayer_event_task(id):
     # lookup user by id and send them a message
     prayer_event = PrayerEvent.objects.get(id=id)
+    absolute_path = str(settings.BASE_DIR) + prayer_event.audio.url
+    pygame.mixer.init()
+    pygame.mixer.music.set_volume(0.7)
+    sound = pygame.mixer.Sound(absolute_path)
+    sound.play()
+    pygame.time.wait(int(sound.get_length() * 1000))
