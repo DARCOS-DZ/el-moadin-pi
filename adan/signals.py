0401549a1617f44f1b1996bf907e162fe78d02fc
S-Amine <saidi.amine.p@gmail.com>
2022-12-31 15:31:00 +0100

    clean the imports

--- adan/signals.py
+++ adan/signals.py
@@ -1,19 +1,19 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import *
-from django.conf import settings
 from constance.signals import config_updated
 from datetime import datetime, timedelta
-from django.utils import timezone
+from .utils import get_todays_prayers
+from constance import config
+from django.core.files import File
+import wget
+from pathlib import Path
 
 @receiver(post_save, sender=LiveEvent)
 def live_event_signal(sender, instance, **kwargs):
-    import wget
-    from pathlib import Path
-    from django.core.files import File
     audio = str(instance.audio)
     try:
-        from .tasks import live_event_task, prayer_audio_task
+        from .tasks import live_event_task
         download  = wget.download(audio, out="media")
         downloaded_file = str(download).encode()
         path = Path(downloaded_file.decode("utf-8"))
@@ -27,28 +27,16 @@ def live_event_signal(sender, instance, **kwargs):
 
 @receiver(post_save, sender=PrayerAudio)
 def prayer_audio_signal(sender, instance, **kwargs):
-    import wget
-    from pathlib import Path
-    from django.core.files import File
     audio = str(instance.audio)
     try:
         from .tasks import prayer_audio_task
-        from .utils import get_todays_prayers
+
         download  = wget.download(audio, out="media")
         downloaded_file = str(download).encode()
         path = Path(downloaded_file.decode("utf-8"))
         with path.open(mode='rb') as f:
             instance.audio = File(f, name=path.name)
             instance.save()
-        # Calcule adan day
-        # current_date_mounth = datetime.today().month
-        # current_date_day = datetime.today().day
-        # prayers = get_todays_prayers()
-        # prayer = prayers[instance.prayer]
-        # adan_time = datetime.strptime(prayer, '%H:%M:%S').time()
-        # print("Todays Prayer time :", prayer)
-        # schedule_adan = datetime(year=2022, month=current_date_mounth, day=current_date_day, hour=adan_time.hour, minute=adan_time.minute, second=adan_time.second)
-        # prayer_audio_task(instance.id, schedule=schedule_adan)
 
     except Exception as e:
         pass
@@ -57,8 +45,6 @@ def prayer_audio_signal(sender, instance, **kwargs):
 def constance_updated(sender, key, old_value, new_value, **kwargs):
     print(sender, key, old_value, new_value)
     if key == "offset_time":
-        from constance import config
-        from .utils import get_todays_prayers
         prayer_source=json.loads(config.PRAYER_SOURCE)["data"]
         new_dictionary = {"data":[]}
         for prayer in prayer_source :
