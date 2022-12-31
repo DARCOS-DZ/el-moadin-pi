9f7e7072ecf4e40eed842f605d07e6745eb33265
S-Amine <saidi.amine.p@gmail.com>
2022-12-31 15:06:52 +0100

    make the topic name & the clind_id == SERIAL_NUMBER

--- mqtt/mqtt_client.py
+++ mqtt/mqtt_client.py
@@ -1,14 +1,15 @@
 import paho.mqtt.client as mqtt
 import json
 from datetime import datetime
+from django.conf import settings
 # The callback for when the client receives a CONNACK response from the server.
 
 def on_connect(client, userdata, flags, rc):
-    print("\n",datetime.now(), "  ", "Connected with result code "+str(rc) + "\n")
+    print("\n",datetime.now(), "  ", "Connected to raspberry_pi/{} with result code ".format(settings.SERIAL_NUMBER)+str(rc) + "\n")
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("raspberry_pi/#", qos=1)
+    client.subscribe("raspberry_pi/{}".format(settings.SERIAL_NUMBER), qos=1)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -39,7 +40,7 @@ def on_message(client, userdata, msg):
 
 
 def main():
-    client = mqtt.Client(client_id='ahmed', clean_session=False)
+    client = mqtt.Client(client_id=str(settings.SERIAL_NUMBER), clean_session=False)
     client.on_connect = on_connect
     client.on_message = on_message
     client.connect_async("142.44.163.144", 1883, 60)
--- mqtt/mqtt_publisher.py
+++ mqtt/mqtt_publisher.py
@@ -2,17 +2,18 @@ import paho.mqtt.client as mqtt
 from .models import *
 import json
 from datetime import datetime
+from django.conf import settings
 # The callback for when the client receives a CONNACK response from the server.
 
 def on_connect(client, userdata, flags, rc):
-    print("\n",datetime.now(), "  ", "Connected with result code "+str(rc) + "\n")
+    print("\n",datetime.now(), "  ", "Connected to raspberry_pi/{} with result code ".format(settings.SERIAL_NUMBER)+str(rc) + "\n")
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("raspberry_pi/#", qos=1)
+    client.subscribe("raspberry_pi/{}".format(settings.SERIAL_NUMBER), qos=1)
 
 def main(topic, message):
-    client = mqtt.Client(client_id='1', clean_session=False)
+    client = mqtt.Client(client_id=str(settings.SERIAL_NUMBER), clean_session=False)
     client.on_connect = on_connect
     client.connect("142.44.163.144", 1883, 60)
     client.publish(topic, message, qos=1)
