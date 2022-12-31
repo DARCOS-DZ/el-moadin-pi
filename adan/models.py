13e91205e48838a562c46289bf0a88f21b89d1fd
S-Amine <saidi.amine.p@gmail.com>
2022-12-31 15:26:08 +0100

    clean Adan models

--- adan/models.py
+++ adan/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import User
-import json
 import mutagen
-from constance import config
-from datetime import datetime, timedelta
+from datetime import datetime
 PRAYER= (('elfajer','elfajer'),('duhr','duhr'),('alasr','alasr'),('almaghreb','almaghreb'),('alaicha','alaicha'))
 
 class PrayerAudio(models.Model):
@@ -24,35 +21,9 @@ class LiveEvent(models.Model):
     name = models.CharField( max_length=120,null=True)
     audio_duration = models.PositiveIntegerField(null=True,blank=True)
     created_at = models.DateTimeField(auto_now=True)
-    # def save(self, *args, **kwargs):
-    #    if self.audio_duration is None or self.audio_duration == "":
-    #     audio_info = mutagen.File(self.audio).info
-    #     self.audio_duration=int(audio_info.length)
-    #    super(LiveEvent(), self).save(*args, **kwargs) # Call the real save() method
-    # def __str__(self):
-    #     return self.id
 
-class State(models.Model):
-    name = models.CharField("state name", max_length=120)
-    offset_time = models.IntegerField()
-    prayer_time = models.JSONField("prayer data time",null=True,blank=True)
     def __str__(self):
-        return self.name
-    def save(self, *args, **kwargs):
-        prayer_source=json.loads(config.PRAYER_SOURCE)["data"]
-        new_dictionary = {"data":[]}
-        for prayer in prayer_source :
-            elfajer=(datetime.strptime(prayer["elfajer"], '%H:%M %p')+timedelta(minutes=self.offset_time)).time()
-            duhr=(datetime.strptime(prayer["duhr"], '%H:%M %p')+timedelta(minutes=self.offset_time)).time()
-            alasr=(datetime.strptime(prayer["alasr"], '%H:%M %p')+timedelta(minutes=self.offset_time)).time()
-            almaghreb=(datetime.strptime(prayer["almaghreb"], '%H:%M %p')+timedelta(minutes=self.offset_time)).time()
-            alaicha=(datetime.strptime(prayer["alaicha"], '%H:%M %p')+timedelta(minutes=self.offset_time)).time()
-            chorouk=(datetime.strptime(prayer["chorouk"], '%H:%M %p')+timedelta(minutes=self.offset_time)).time()
-            new_dictionary_item = {"id":prayer["id"],"date":prayer["date"],"elfajer":str(elfajer),"chorouk":str(chorouk),"duhr":str(duhr),"alasr":str(alasr),"almaghreb":str(almaghreb),"alaicha":str(alaicha)}
-            new_dictionary["data"].append(new_dictionary_item)
-            self.prayer_time=new_dictionary
-            print(alasr)
-        super(State, self).save(*args, **kwargs) # Call the real save() method
+        return self.id
 
 class PrayerEvent(models.Model):
     TYPE = (('after','after'),('before','before'))
@@ -68,15 +39,3 @@ class PrayerEvent(models.Model):
        super(PrayerEvent, self).save(*args, **kwargs) # Call the real save() method
     def __str__(self):
         return f"{self.type}-{self.prayer}"
-
-class Mosque(models.Model):
-    topic = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
-    name = models.CharField("name of mosque", max_length=150)
-    status = models.BooleanField(default=False)
-    state = models.ForeignKey(State, on_delete=models.CASCADE,null=True,blank=True)
-    def __str__(self):
-        return f"{self.name} is {self.status} now"
-class Topic(models.Model):
-    serial_number = models.CharField(max_length=300,unique=True)
-    def __str__(self):
-        return  self.serial_number
