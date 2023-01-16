f3cf2a2aca0c022ebba766d00ac7ac0df55813f7
S-Amine <saidi.amine.p@gmail.com>
2023-01-16 14:40:52 +0100

    add PrayerEvent to admin

--- adan/admin.py
+++ adan/admin.py
@@ -1,2 +1,8 @@
 from django.contrib import admin
+from .models import *
 # Register your models here.
+
+class PrayerEventAdmin(admin.ModelAdmin):
+    list_display = ('type', 'name', 'prayer', 'audio', 'audio_duration', 'created_at')
+
+admin.site.register(PrayerEvent, PrayerEventAdmin)
