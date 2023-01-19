513f15f5e309e8385668343a319d171ec5a58e4c
S-Amine <saidi.amine.p@gmail.com>
2023-01-19 09:08:21 +0100

    put the models in the Admin interface

--- adan/admin.py
+++ adan/admin.py
@@ -5,4 +5,10 @@ from .models import *
 class PrayerEventAdmin(admin.ModelAdmin):
     list_display = ('type', 'name', 'prayer', 'audio', 'audio_duration', 'created_at')
 
+class PostCodesAdmin(admin.ModelAdmin):
+    exclude = ('pcname',)  #This exclude function will do which u want!
+    list_display = ('id', 'gisid', 'title', )
+
 admin.site.register(PrayerEvent, PrayerEventAdmin)
+admin.site.register(LiveEvent)
+admin.site.register(PrayerAudio)
