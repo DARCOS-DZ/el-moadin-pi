af4185bf67cd8b672c73837b5d87ded72145a51b
S-Amine <saidi.amine.p@gmail.com>
2023-01-19 15:23:02 +0100

    add allowed hosts

--- el_moadin_pi/settings.py
+++ el_moadin_pi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-&eso57mwp7o#%@_6v%d=x2daozw!l*%d(3hxfc%45-atr*4p+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
