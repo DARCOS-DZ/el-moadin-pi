a370caab85273df8ec449afd84dc8b183b82727f
S-Amine <saidi.amine.p@gmail.com>
2023-01-24 14:49:08 +0100

    create zigbee functions

--- adan/utils.py
+++ adan/utils.py
@@ -1,5 +1,7 @@
 from constance import config
 from datetime import datetime
+import requests
+import json
 
 def get_todays_prayers():
     current_date_mounth = datetime.today().month
@@ -7,3 +9,28 @@ def get_todays_prayers():
     id_day = (current_date_mounth-1)*30+current_date_day # get current day id
     prayers = config.PrayerTime["data"][id_day]
     return prayers
+
+def get_zigbee_state():
+    url = f"{config.home_assistant_address}/api/states/{config.entity_id}"
+    payload={}
+    headers = {
+      'Authorization': f'Bearer {config.home_assistant_token}'
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    object = response.json()
+    return object["state"]
+
+def zigbee_switch(state=""):
+    if state == "on" :
+        url = f"{config.home_assistant_address}/api/services/switch/turn_on"
+    if state == "off" :
+        url = f"{config.home_assistant_address}/api/services/switch/turn_off"
+    payload = json.dumps({
+      "entity_id": config.entity_id
+    })
+    headers = {
+      'Content-Type': 'application/json',
+      'Authorization': f'Bearer {config.home_assistant_token}'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    print(response.text)
