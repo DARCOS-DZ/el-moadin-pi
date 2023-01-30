3c12bdd7019edc06e79c93b502db71c5a35d3aa4
S-Amine <saidi.amine.p@gmail.com>
2023-01-30 20:49:56 +0100

    debug

--- el_moadin_pi/settings.py
+++ el_moadin_pi/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&eso57mwp7o#%@_6v%d=x2daozw!l*%d(3hxfc%45-atr*4p+("
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["localhost", "*"]
 
 
 # Application definition
