c6705d33b20f00ffd264dd77089d654509610ed8
S-Amine <saidi.amine.p@gmail.com>
2023-02-01 11:11:19 +0100

    Change the mqtt strategy

--- mqtt/apps.py
+++ mqtt/apps.py
@@ -1,13 +1,9 @@
 from django.apps import AppConfig
-import os
 import time
-import background
 
 class MqttConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mqtt"
-    @background.task
     def ready(self):
-        from el_moadin_pi.utils import background_loop
-        if os.environ.get('RUN_MAIN') == "true" :
-            background_loop()
+        from el_moadin_pi.utils import check
+        check()
