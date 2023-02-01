f94637ffe8c584cfcd30328d33ed906ef6efb9ab
S-Amine <saidi.amine.p@gmail.com>
2023-02-01 19:49:47 +0100

    try

--- adan/utils.py
+++ adan/utils.py
@@ -11,33 +11,40 @@ def get_todays_prayers():
     return prayers
 
 def get_zigbee_state():
-    url = f"{config.home_assistant_address}/api/states/{config.entity_id}"
-    payload={}
-    headers = {
-      'Authorization': f'Bearer {config.home_assistant_token}'
-    }
-    response = requests.request("GET", url, headers=headers, data=payload)
-    object = response.json()
-    return object["state"]
+    try:
+        url = f"{config.home_assistant_address}/api/states/{config.entity_id}"
+        payload={}
+        headers = {
+          'Authorization': f'Bearer {config.home_assistant_token}'
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+        object = response.json()
+        return object["state"]
+    except:
+        pass
+
 
 def zigbee_switch(state=""):
-    if state == "on" :
-        url = f"{config.home_assistant_address}/api/services/switch/turn_on"
-    elif state == "off" :
-        url = f"{config.home_assistant_address}/api/services/switch/turn_off"
-    else :
-      state = get_zigbee_state()
-      if state == "on" :
-          url = f"{config.home_assistant_address}/api/services/switch/turn_off"
-      elif state == "off" :
-          url = f"{config.home_assistant_address}/api/services/switch/turn_on"
+    try:
+        if state == "on" :
+            url = f"{config.home_assistant_address}/api/services/switch/turn_on"
+        elif state == "off" :
+            url = f"{config.home_assistant_address}/api/services/switch/turn_off"
+        else :
+          state = get_zigbee_state()
+          if state == "on" :
+              url = f"{config.home_assistant_address}/api/services/switch/turn_off"
+          elif state == "off" :
+              url = f"{config.home_assistant_address}/api/services/switch/turn_on"
 
-    payload = json.dumps({
-      "entity_id": config.entity_id
-    })
-    headers = {
-      'Content-Type': 'application/json',
-      'Authorization': f'Bearer {config.home_assistant_token}'
-    }
-    response = requests.request("POST", url, headers=headers, data=payload)
-    print(response.text)
+        payload = json.dumps({
+          "entity_id": config.entity_id
+        })
+        headers = {
+          'Content-Type': 'application/json',
+          'Authorization': f'Bearer {config.home_assistant_token}'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        print(response.text)
+    except:
+        pass 
