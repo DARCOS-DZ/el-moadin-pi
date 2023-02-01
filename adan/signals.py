4a47078be8fa45be91c0109aa977e0ecfde6d295
S-Amine <saidi.amine.p@gmail.com>
2023-02-01 16:45:58 +0100

    debug

--- adan/signals.py
+++ adan/signals.py
@@ -3,7 +3,6 @@ from django.db.models.signals import post_save
 from .models import *
 from constance.signals import config_updated
 from datetime import datetime, timedelta
-from .utils import get_todays_prayers
 from constance import config
 from django.core.files import File
 import wget
@@ -55,6 +54,7 @@ def prayer_event_signal(sender, instance, **kwargs):
 
 @receiver(config_updated)
 def constance_updated(sender, key, old_value, new_value, **kwargs):
+    from adan.utils import get_todays_prayers
     print(sender, key, old_value, new_value)
     current_tz = timezone.get_current_timezone()
     if key == "offset_time":
