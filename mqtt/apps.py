5438bc0e45d694ae522452dd454c764730002ccc
S-Amine <saidi.amine.p@gmail.com>
2023-02-01 11:33:14 +0100

    clean the code

--- mqtt/apps.py
+++ mqtt/apps.py
@@ -5,5 +5,5 @@ class MqttConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "mqtt"
     def ready(self):
-        from el_moadin_pi.utils import check
-        check()
+        from el_moadin_pi.utils import background_loop
+        background_loop()
--- mqtt/mqtt_client.py
+++ mqtt/mqtt_client.py
@@ -42,65 +42,60 @@ def on_connect(client, userdata, flags, rc):
     except :
         pass
 
-
 def on_disconnect(client, userdata, rc):
     client.reconnect()
 
 # The callback for when a PUBLISH message is received from the server.
 
 def on_message(client, userdata, msg):
-    try:
-        brut_text = str(msg.payload.decode("utf-8"))
-        json_msg = json.loads(brut_text)
-        if json_msg["sender"] == 0:
-            if json_msg["operation"] == "transfer":
-                if json_msg["data"]["model"] == "PrayerAudio":
-                    from adan.models import PrayerAudio
-                    prayer_audio = PrayerAudio(audio=json_msg["data"]["audio"], prayer=json_msg["data"]["prayer"], audio_duration=json_msg["data"]["audio_duration"])
-                    prayer_audio.save()
-                    now = datetime.now()
-                    print("Prayer audio :", json_msg["data"]["audio"], "recorded for", json_msg["data"]["prayer"], "prayer", "with a duration of", json_msg["data"]["audio_duration"])
-                if json_msg["data"]["model"] == "LiveEvent":
-                    from adan.models import LiveEvent
-                    live_envent = LiveEvent(audio=json_msg["data"]["audio"], audio_duration=json_msg["data"]["audio_duration"])
-                    live_envent.save()
-                    now = datetime.now()
-                    print("Live Event :", live_envent.audio, "with a duration of", live_envent.audio_duration, "has been successfully recorded")
-                if json_msg["data"]["model"] == "PrayerEvent":
-                    from adan.models import PrayerEvent
-                    prayer_event = PrayerEvent(type=json_msg["data"]["type"], prayer=json_msg["data"]["prayer"], audio=json_msg["data"]["audio"], audio_duration=json_msg["data"]["audio_duration"], created_at=datetime(2023, 1, 5, tzinfo=timezone.get_current_timezone()))
-                    prayer_event.save()
-                    now = datetime.now()
-                    print("Prayer Event :", prayer_event.type, prayer_event.prayer, "with audio file =", prayer_event.audio, "with a duration of :", prayer_event.audio_duration, "has been successfully recorded")
-                if json_msg["data"]["model"] == "constance":
-                    config.offset_time = json_msg["data"]["offset_time"]
-                    print("Done")
-                if json_msg["data"]["model"] == "Plug":
-                    state = json_msg["data"]["state"]
-                    zigbee_switch(state)
-                    print(f"Plug switched to {state}")
-                    json_msg={
-                	"operation": "transfer",
-                	"sender": 1,
-                	"data": {
-                        "model": "Plug",
-                		"state": get_zigbee_state(),
-                        "date": str(datetime.now()),
-                	    }
-                    }
-                    message = json.dumps(json_msg,ensure_ascii=False)
-                    client.publish(topic, message, qos=1)
-                    print(json_msg)
-        else :
-          pass
-    except Exception as e:
-        print(e)
+    brut_text = str(msg.payload.decode("utf-8"))
+    json_msg = json.loads(brut_text)
+    if json_msg["sender"] == 0:
+        if json_msg["operation"] == "transfer":
+            if json_msg["data"]["model"] == "PrayerAudio":
+                from adan.models import PrayerAudio
+                prayer_audio = PrayerAudio(audio=json_msg["data"]["audio"], prayer=json_msg["data"]["prayer"], audio_duration=json_msg["data"]["audio_duration"])
+                prayer_audio.save()
+                now = datetime.now()
+                print("Prayer audio :", json_msg["data"]["audio"], "recorded for", json_msg["data"]["prayer"], "prayer", "with a duration of", json_msg["data"]["audio_duration"])
+            if json_msg["data"]["model"] == "LiveEvent":
+                from adan.models import LiveEvent
+                live_envent = LiveEvent(audio=json_msg["data"]["audio"], audio_duration=json_msg["data"]["audio_duration"])
+                live_envent.save()
+                now = datetime.now()
+                print("Live Event :", live_envent.audio, "with a duration of", live_envent.audio_duration, "has been successfully recorded")
+            if json_msg["data"]["model"] == "PrayerEvent":
+                from adan.models import PrayerEvent
+                prayer_event = PrayerEvent(type=json_msg["data"]["type"], prayer=json_msg["data"]["prayer"], audio=json_msg["data"]["audio"], audio_duration=json_msg["data"]["audio_duration"], created_at=datetime(2023, 1, 5, tzinfo=timezone.get_current_timezone()))
+                prayer_event.save()
+                now = datetime.now()
+                print("Prayer Event :", prayer_event.type, prayer_event.prayer, "with audio file =", prayer_event.audio, "with a duration of :", prayer_event.audio_duration, "has been successfully recorded")
+            if json_msg["data"]["model"] == "constance":
+                config.offset_time = json_msg["data"]["offset_time"]
+                print("Done")
+            if json_msg["data"]["model"] == "Plug":
+                state = json_msg["data"]["state"]
+                zigbee_switch(state)
+                print(f"Plug switched to {state}")
+                json_msg={
+            	"operation": "transfer",
+            	"sender": 1,
+            	"data": {
+                    "model": "Plug",
+            		"state": get_zigbee_state(),
+                    "date": str(datetime.now()),
+            	    }
+                }
+                message = json.dumps(json_msg,ensure_ascii=False)
+                client.publish(topic, message, qos=1)
+                print(json_msg)
 
 # @background.task
 def main():
     client = mqtt.Client(client_id=str(settings.SERIAL_NUMBER), clean_session=False)
     client.on_connect = on_connect
     client.on_message = on_message
+    client.on_disconnect = on_disconnect
     client.connect_async(config.broker_ip, port=1883)
     client.loop_start()
     return client
