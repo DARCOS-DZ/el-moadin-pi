e37a964d09fa90d2cb138afa3cf7e41e15ac916b
S-Amine <saidi.amine.p@gmail.com>
2023-02-25 19:43:48 +0100

    little change

--- mqtt/mqtt_client.py
+++ mqtt/mqtt_client.py
@@ -85,18 +85,21 @@ def background_loop():
             # Print the error message
             print("Error occurred while processing the message")
 
-    def on_disconnect(client, userdata, rc):
-        print(f"Disconnected from MQTT broker with result code {rc}")
-        time.sleep(5)  # Wait for 5 seconds before attempting to reconnect
-        client.reconnect()
+    # def on_disconnect(client, userdata, rc):
+    #     print(f"Disconnected from MQTT broker with result code {rc}")
+    #     time.sleep(5)  # Wait for 5 seconds before attempting to reconnect
+    #     client.reconnect()
 
     while True:
-        client = mqtt.Client(client_id=str(settings.SERIAL_NUMBER), clean_session=True)
+        client = mqtt.Client(client_id=settings.SERIAL_NUMBER, clean_session=True)
         client.on_connect = on_connect
         client.on_message = on_message
-        client.on_disconnect = on_disconnect
-        client.connect(config.broker_ip, port=1883)
-        client.loop_forever()
+        # client.on_disconnect = on_disconnect
+        client.connect(config.broker_ip, port=1883, keepalive=60)
+        # Force the client to reconnect in case of a disconnection
+        while True:
+            try:
+                client.loop_forever
 
 if __name__ == '__main__':
     background_loop()
